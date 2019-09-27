a7f61a1e983752a74cb293bdc574890a507497a9
jmishev <53108707+jmishev@users.noreply.github.com>
2019-09-27 13:39:24 +0300

    MSQL syntax fised

--- MySQL.py
+++ MySQL.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="tik-71.Lion",
+    password="XXXXXXXXX",
     database="inventory",
     )
 
